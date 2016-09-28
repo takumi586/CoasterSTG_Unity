396970949c6d0ffbf317c629f74fe5aacb540b1e
ryoji7583 <ryoji.75830923@gmail.com>
2016-09-28 23:24:44 +0900

    Buttonのクリックでシーンが移動
    スタートボタンを押すとゲームシーンへ移動するスクリプトの作成。

---
+++ Assets/Fujimoto/F_script/F_SceneMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class F_SceneMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void ButtonClick(){
+		SceneManager.LoadScene ("F_Game");
+	}
+
+}
