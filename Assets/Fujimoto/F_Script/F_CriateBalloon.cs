c931c8b7683bd37a15190a3a80b92b92aaca6493
ryoji7583 <ryoji.75830923@gmail.com>
2016-10-09 16:50:26 +0900

    Balloonの追加
    Balloonプレハブを作成し、自動で上昇かつランダム生成できるようにした。

---
+++ Assets/Fujimoto/F_Script/F_CriateBalloon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class F_CriateBalloon : MonoBehaviour {
+
+	public GameObject Balloon;
+	public GameObject Vehicle;
+	public float timeOut;
+	private float timeElapsed;
+	private Vector3 pos;
+
+	void Start(){
+	}
+
+	void Update(){
+		timeElapsed += Time.deltaTime;
+
+		if(timeElapsed >= timeOut){
+			float x = Random.Range (-7.0f, 7.0f);
+			float y = Random.Range (5.5f, 6.0f);
+			float z = Random.Range (-2.0f, 2.0f);
+			Vector3 pos = Vehicle.transform.position;
+			Instantiate (Balloon, new Vector3(pos.x + x, y, z), Quaternion.identity);
+			timeElapsed = 0.0f;
+		}
+	}
+
+}
---
+++ Assets/Fujimoto/F_Script/F_Target.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class F_Target : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "F_Bullet") {
+			Destroy(this.gameObject);
+		}
+	}
+}
