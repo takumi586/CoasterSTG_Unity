c19ec15bc48a896f404b4a58ad172d0f9c6a2f15
e1b14069 <thedekopin0108@gmail.com>
2016-10-10 17:48:26 +0900

    トロッコのテクスチャの追加
    AssetStoreでfreeのテクスチャをインポートした。

---
+++ Assets/N_Course/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public float speed = 0.02f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (speed, 0, 0);
+
+	}
+}
