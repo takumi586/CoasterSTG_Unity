c818f0aef456cf1533a6584d69ec191fe39612e1
e1b14069 <thedekopin0108@gmail.com>
2016-10-12 03:05:20 +0900

    背景一つ目の設置
    Assetからとった3Dの建物を配した。

---
+++ Assets/N_Background/N_TramMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class N_TramMove : MonoBehaviour {
+	public float speed = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, 0, speed);
+	}
+}
+
