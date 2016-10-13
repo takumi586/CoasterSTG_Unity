1812ff4c9b1b6f0cf4c16ad2ec10cd7bd5ac940f
e1b14069 <thedekopin0108@gmail.com>
2016-10-13 19:35:59 +0900

    Conflictの解消
    共通していたシーンを削除

--- Assets/N_vehicle/N_move.cs
+++ Assets/N_vehicle/N_move.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class N_Move : MonoBehaviour {
+public class N_move : MonoBehaviour {
 	public float speed = 0.01f;
 
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (speed, 0, 0);
