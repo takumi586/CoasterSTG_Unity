582851b97a7462d4e1e1353c52da9d4ba7f3bd75
e1b14069 <thedekopin0108@gmail.com>
2016-10-20 13:17:02 +0900

    SEの鳴らす
    フリー素材をインポートし、それを鳴らせるようにプログラムを作成した。

---
+++ Assets/Nsimura/N_SECreate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class N_SECreate: MonoBehaviour {
+
+	public AudioSource shootSound;
+
+	// Use this for initialization
+	void Start () {
+		
+	shootSound = GetComponent<AudioSource>();
+	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.K)) {
+			shootSound.PlayOneShot (shootSound.clip);
+		} 
+	}
+}
