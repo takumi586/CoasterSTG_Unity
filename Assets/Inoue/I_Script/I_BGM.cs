063a2efacffac9947dcbca8f93c2e631027352b9
takumi586 <TakumiInoue0495417@gmail.com>
2016-10-25 22:32:48 +0900

    bgm制御スクリプトの作成
    ・音量
    ・開始から何フレーム後に再生
    ・なんフレーム後に、何秒間でフェードアウトを行うか  など
    また、ステージbgmの完成版をインポートした

---
+++ Assets/Inoue/I_Script/I_BGM.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class I_BGM : MonoBehaviour {
+	private int cnt = 0;
+	public float volume = 1;
+	public int startFlame = -1;
+	public int endFlame = -1;
+	public float endSec = 0;
+	public bool endFlag = false;
+	public bool destroy = false;
+	private AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource>();
+		audio.volume = volume;
+	}
+
+	void FixedUpdate () {
+		audio.volume = volume;
+		if (startFlame >= 0 && startFlame <= cnt && !audio.isPlaying) {
+			audio.Play ();
+			startFlame = -1;
+		}
+		if ((endFlame >= 0 && endFlame <= cnt && audio.isPlaying) || endFlag) {
+			if (endSec == 0) {
+				volume = 0;
+			} else {
+				volume -= 1 / (50 * endSec);
+			}
+			if (volume <= 0) {
+				audio.Stop ();
+				endFlame = -1;
+				if (destroy)
+					Destroy (this.gameObject);
+			}
+		}
+		cnt++;
+	}
+}
