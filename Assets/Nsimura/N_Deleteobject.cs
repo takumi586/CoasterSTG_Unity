3c7b11aa7bc05928a82a374d17cd80b6372cfef2
e1b14069 <thedekopin0108@gmail.com>
2016-10-26 13:26:08 +0900

    オブジェクト削除スクリプトを作成
    ステージ２のオブジェクトを削除するスクリプトを作成、アタッチした。

---
+++ Assets/Nsimura/N_Deleteobject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class N_Deleteobject: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "N_Tram") {
+			Delete ();
+		}
+	}
+
+
+	void Delete(){
+		GameObject obj = GameObject.Find ("N_House");
+		GameObject.Destroy (obj);
+	}
+}
