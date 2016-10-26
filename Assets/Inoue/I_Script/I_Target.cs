f3995fdb35c31b921e94298de229a345956658b8
takumi586 <TakumiInoue0495417@gmail.com>
2016-10-26 20:21:28 +0900

    BGMとSEの調整

--- Assets/Inoue/I_Script/I_Target.cs
+++ Assets/Inoue/I_Script/I_Target.cs
@@ -4,29 +4,42 @@ using System.Collections;
 public class I_Target : MonoBehaviour {
 	public int ScorePoint;
 	private I_Score s;
+	private bool seFlag = false;
 
 	// 効果音
-	public AudioClip Se;
 	public float seVolume = 1;
+	public int destroyCnt = 0;
+	private int cnt = 0;
+	private AudioSource audioSe;
 
 	void Start() {
 		s = GameObject.Find ("ScoreDisplay").GetComponent<I_Score> ();
+		audioSe = GetComponent<AudioSource>();
+		audioSe.volume = seVolume;
+	}
+
+	void FixedUpdate() {
+		if (seFlag) {
+			if (cnt >= destroyCnt) {
+				Destroy (this.gameObject);
+			}
+			cnt++;
+		}
 	}
 		
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag == "I_Bullet") {
-			if (ScorePoint >= 0) {
-				s.addScore (ScorePoint);
-			} else {
-				s.subScore (-ScorePoint);
+		if (!seFlag) {
+			if (other.gameObject.tag == "I_Bullet") {
+				if (ScorePoint >= 0) {
+					s.addScore (ScorePoint);
+				} else {
+					s.subScore (-ScorePoint);
+				}
+				audioSe.Play ();
+				seFlag = true;
+				Destroy (other.gameObject);
+				this.transform.Translate (0f, -100f, 0f);
 			}
-			PlaySe ();
-			Destroy(this.gameObject);
-			Destroy (other.gameObject);
 		}
-	}
-
-	void PlaySe() {
-		AudioSource.PlayClipAtPoint (Se, transform.position, seVolume);
 	}
 }
